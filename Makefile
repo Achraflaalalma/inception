cfce18170139cf7a452bb48ef0e9fdcd01e2c53a
Achraflaalalma <achrfalalix@gmail.com>
2025-02-23 19:45:17 +0000

    added ftp service

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ up:
 
 # Stop and remove containers
 down:
-	cd srcs/ && docker compose down --remove-orphans --volumes
+	cd srcs/ && docker compose down --remove-orphans
 
 # Build images
 build:
@@ -16,26 +16,6 @@ re: down up
 # Show running containers
 ps:
 	cd srcs/ && docker compose ps
-
-# Create the Docker secret
-secrets:
-	@echo "Creating secrets..."
-	@docker secret create admin_pass srcs/secrets/admin_pass.txt
-	@docker secret create db_pass srcs/secrets/db_pass.txt
-	@docker secret create user_pass srcs/secrets/user_pass.txt
-
-#swarm init
-swarm:
-	docker swarm init
-
-#swarm leave
-swarm-leave:
-	docker swarm leave --force
-
-# Remove the Docker secret
-rm-secrets:
-	@echo "Removing secrets..."
-	@docker secret rm admin_pass db_pass user_pass
 
 # Remove all containers, networks, and volumes
 clean: down
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,8 +5,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$(cat /run/secrets/db_pass) --dbhost=$DB_HOST --allow-root
-wp core install --url=$DOMAIN --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$(cat /run/secrets/admin_pass) --admin_email=$ADMIN_EMAIL --allow-root
-wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$(cat /run/secrets/user_pass) --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
+wp core install --url=$DOMAIN --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASS --allow-root
 
 php-fpm7.4 -F
